551679b3a3c990fac62765e9796b91ff13f0bc0f
AlexMaksto <alexstolyaruk@gmail.com>
2023-08-01 21:30:00 +0300

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {baseURL} from "../constans";
 
 const token = process.env.REACT_APP_API_READ_ACCESS_TOKEN;
+// const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTA5M2I3MjQ1Y2M0NzBlNjZiNDIzY2IzMzcyOTViOCIsInN1YiI6IjY0YzI0ZjdmZGI0ZWQ2MDBlNGNhY2JkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.djZPp6QbPAzPd1cg5H28lYhh_ITobDtEYtayHMpNjXg';
 // const apiKey = process.env.REACT_APP_API_KEY;
 
 const config = {
